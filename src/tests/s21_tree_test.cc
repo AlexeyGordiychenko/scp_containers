48c9e4d73470b7a0b6f6e68359a2af55c72235d5
elidacon <elidacon@student.21-school.ru>
2023-10-09 21:01:28 +0500

    Add extra test for the tree validation with duplicates

--- src/tests/s21_tree_test.cc
+++ src/tests/s21_tree_test.cc
@@ -37,3 +37,28 @@ TEST(TreeValidation, 0) {
   EXPECT_TRUE(tree1.is_valid_tree());
   EXPECT_TRUE(tree2.is_valid_tree());
 }
+
+TEST(TreeValidation, 1) {
+  s21::RbTree<int, std::pair<const int, std::string>, GetKeyMap, std::less<int>>
+      tree1, tree2;
+
+  for (int i = 0; i < 100000; ++i) {
+    int key = rand() % 1000;
+    tree1.insert(std::make_pair(key, std::to_string(key)), true);
+  }
+  EXPECT_TRUE(tree1.is_valid_tree());
+  for (auto elem : tree1) {
+    if (elem.first % 2 == 0) {
+      tree1.erase(tree1.find(elem.first));
+    }
+  }
+  EXPECT_TRUE(tree1.is_valid_tree());
+  for (int i = 0; i < 100000; ++i) {
+    int key = rand() % 1000;
+    tree2.insert(std::make_pair(key, std::to_string(key)));
+  }
+  EXPECT_TRUE(tree2.is_valid_tree());
+  tree1.merge(tree2, true);
+  EXPECT_TRUE(tree1.is_valid_tree());
+  EXPECT_TRUE(tree2.is_valid_tree());
+}
