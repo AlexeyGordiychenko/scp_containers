23fc89a0f952fe3f3afdb2b139d081c56b61d127
elidacon <elidacon@student.21-school.ru>
2023-10-10 23:18:34 +0500

    Add multiset implementation and tests for it

---
+++ src/s21_multiset.h
@@ -0,0 +1,128 @@
+#ifndef S21_MULTISET_H_
+#define S21_MULTISET_H_
+
+#include <vector>
+
+#include "s21_rb_tree.h"
+
+namespace s21 {
+
+template <class Key, class Compare = std::less<Key>>
+class multiset final {
+ private:
+  struct GetKey {
+    const Key operator()(const Key& value) const { return value; }
+  };
+  using BalancedTree = RbTree<Key, Key, GetKey, Compare>;
+
+ public:
+  using key_type = Key;
+  using value_type = Key;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using iterator = class BalancedTree::iterator;
+  using const_iterator = class BalancedTree::const_iterator;
+  using size_type = size_t;
+
+  // constructors and destructor
+  multiset() : tree_() {}
+  multiset(const std::initializer_list<value_type>& items) : tree_() {
+    for (const value_type& item : items) {
+      tree_.insert(item, true);
+    }
+  }
+  multiset(const multiset& ms) : tree_(ms.tree_) {}
+  multiset(multiset&& ms) : tree_(std::move(ms.tree_)) {}
+  ~multiset() {}
+
+  // assignment operator oveload for copying an object
+  multiset& operator=(const multiset& ms) {
+    tree_ = ms.tree_;
+    return *this;
+  }
+
+  // assignment operator oveload for moving an object
+  multiset& operator=(multiset&& ms) noexcept {
+    tree_ = std::move(ms.tree_);
+    return *this;
+  }
+
+  // iterators
+  iterator begin() noexcept { return tree_.begin(); }
+  const_iterator cbegin() const noexcept { return tree_.cbegin(); }
+  std::reverse_iterator<iterator> rbegin() noexcept { return tree_.rbegin(); }
+  std::reverse_iterator<const_iterator> crbegin() const noexcept {
+    return tree_.crbegin();
+  }
+  iterator end() noexcept { return tree_.end(); }
+  const_iterator cend() const noexcept { return tree_.cend(); }
+  std::reverse_iterator<iterator> rend() noexcept { return tree_.rend(); }
+  std::reverse_iterator<const_iterator> crend() const noexcept {
+    return tree_.crend();
+  }
+
+  // capacity
+  bool empty() const noexcept { return tree_.empty(); }
+  size_type size() const noexcept { return tree_.size(); }
+  size_type max_size() const noexcept { return tree_.max_size(); }
+
+  // set modifiers
+  void clear() { tree_.clear(); }
+  iterator insert(const_reference value) {
+    return tree_.insert(value, true).first;
+  }
+  void erase(iterator pos) { tree_.erase(pos); }
+  void swap(multiset& other) noexcept { tree_.swap(other.tree_); }
+  void merge(multiset& other) { tree_.merge(other.tree_, true); }
+
+  template <typename... Args>
+  std::vector<iterator> insert_many(Args&&... args) {
+    std::vector<iterator> inserted;
+    inserted.reserve(sizeof...(args));
+
+    for (auto&& arg : {args...}) {
+      inserted.push_back(insert(arg));
+    }
+
+    return inserted;
+  }
+
+  // set lookup
+  size_type count(const Key& key) const {
+    auto it = tree_.lower_bound(key);
+    size_type count = 0;
+    while (it != tree_.cend() && *it == key) {
+      count++;
+      it++;
+    }
+    return count;
+  }
+  iterator find(const Key& key) { return tree_.find(key); }
+  const_iterator find(const Key& key) const { return tree_.find(key); }
+  bool contains(const Key& key) const {
+    return tree_.find(key) != tree_.cend();
+  }
+  std::pair<const_iterator, const_iterator> equal_range(const Key& key) const {
+    auto it_lower = tree_.lower_bound(key);
+    auto it_upper = it_lower;
+    while (it_upper != tree_.cend() && *it_upper == key) {
+      it_upper++;
+    }
+    return std::make_pair(it_lower, it_upper);
+  }
+  iterator lower_bound(const Key& key) { return tree_.lower_bound(key); }
+  const_iterator lower_bound(const Key& key) const {
+    return tree_.lower_bound(key);
+  }
+  iterator upper_bound(const Key& key) { return tree_.upper_bound(key); }
+  const_iterator upper_bound(const Key& key) const {
+    return tree_.upper_bound(key);
+  }
+
+ private:
+  BalancedTree tree_;
+};
+
+}  // namespace s21
+
+#endif  // S21_MULTISET_H_
